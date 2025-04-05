ef8b79b9a79188a166fb1a28635fb5c102b530fb
Andrew Bush <andrewbush1525@gmail.com>
2025-04-05 12:53:16 -0500

    add edit functionality

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,7 +1,7 @@
-import { Alert, FlatList, StyleSheet } from 'react-native';
+import { Alert, FlatList, StyleSheet, Modal } from 'react-native';
 
 import { Text, View } from '@/components/Themed';
-import { Button, Card, TextInput } from 'react-native-paper';
+import { Button, Card, Portal, TextInput } from 'react-native-paper';
 import { useEffect, useState } from 'react';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import Swipeable from 'react-native-gesture-handler/ReanimatedSwipeable';
@@ -10,7 +10,7 @@ import Reanimated, {
 } from 'react-native-reanimated';
 
 
-const RightActionComponent = ({ progress, dragX }) => {
+const RightActionComponent = ({ progress, dragX }: { progress: any, dragX: any }  ) => {
   const animatedStyle = useAnimatedStyle(() => {
     'worklet';
     return {
@@ -34,6 +34,9 @@ interface Task {
 export default function TabOneScreen() {
   const [task, setTask] = useState("");
   const [tasks, setTasks] = useState<Task[]>([]);
+  const [isEditingTask, setIsEditingTask] = useState<boolean>(false);
+  const [editingTask, setEditingTask] = useState<Task | null>(null);
+  const [editingText, setEditingText] = useState<string>("");
 
   const storeData = async (value: Task[]) => {
     try {
@@ -82,6 +85,23 @@ export default function TabOneScreen() {
     }
   };
 
+  const editTask = (taskIndex: number, newTitle: string) => {
+    console.log("editing task", taskIndex, newTitle);
+    setIsEditingTask(true);
+    setEditingTask(tasks[taskIndex]);
+    setEditingText(newTitle);
+  }
+
+  const handleEditTask = (taskIndex: number, newTitle: string) => {
+    console.log("handled", taskIndex, newTitle);
+    const updatedTasks = tasks.map((task, index) => index === taskIndex ? { ...task, title: newTitle } : task);
+    setTasks(updatedTasks);
+    storeData(updatedTasks);
+    setIsEditingTask(false);
+    setEditingTask(null);
+    setEditingText("");
+  }
+
   const renderTask = (singleTask: Task, taskIndex: number) => (
     <Swipeable
       overshootRight={false}
@@ -90,7 +110,7 @@ export default function TabOneScreen() {
         <RightActionComponent progress={progress} dragX={dragX} />
       )}
     >
-      <Card style={styles.card} mode='outlined'>
+      <Card style={styles.card} mode='outlined' onLongPress={() => editTask(taskIndex, singleTask.title)}>
         <Card.Content>
           <Text>{singleTask.title}</Text>
         </Card.Content>
@@ -98,34 +118,70 @@ export default function TabOneScreen() {
     </Swipeable>
   )
 
+  const renderEditTask = (task: Task | null, taskIndex: number) => (
+    <Portal>
+      <Modal visible={isEditingTask} onDismiss={() => handleEditTask(taskIndex, task?.title || "")} animationType='slide' transparent={true}>
+        <View style={styles.modalContainer}>
+          <View style={styles.modalContent}>
+            <Text style={styles.modalTitle}>Edit Task</Text>
+            <TextInput
+              multiline={true}
+              numberOfLines={4}
+              value={editingText}
+              onChangeText={text => setEditingText(text)}
+              style={styles.textInput}
+              contentStyle={styles.textInputContent}
+              activeOutlineColor="white"
+              mode="outlined"
+            />
+            <Button
+              mode='contained'
+              onPress={() => {
+                if (editingText) {
+                  handleEditTask(taskIndex, editingText)
+                }
+              }}
+              style={styles.button}
+            >
+              <Text>Save</Text>
+            </Button>
+          </View>
+        </View>
+      </Modal>
+    </Portal>
+  )
+
   useEffect(() => {
     getData();
   }, [])
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Schedule</Text>
-      <TextInput
-        multiline={true}
-        numberOfLines={4}
-        mode="outlined"
-        label="Task"
-        value={task}
-        onChangeText={text => setTask(text)}
-        style={styles.textInput}
-        contentStyle={styles.textInputContent}
-        activeOutlineColor="white"
-      />
-      <Button mode='contained' onPress={() => AddTask()} style={styles.button}>
-        <Text>Add Task</Text>
-      </Button>
-      <FlatList
-        data={tasks}
-        keyExtractor={(item, index) => item.id}
-        renderItem={({item, index}) => renderTask(item, index)}
-        style={styles.flatList}
-      />
-    </View>
+    <>
+      <View style={styles.container}>
+        <Text style={styles.title}>Schedule</Text>
+        <TextInput
+          multiline={true}
+          numberOfLines={4}
+          mode="outlined"
+          label="Task"
+          value={task}
+          onChangeText={text => setTask(text)}
+          style={styles.textInput}
+          contentStyle={styles.textInputContent}
+          activeOutlineColor="white"
+        />
+        <Button mode='contained' onPress={() => AddTask()} style={styles.button}>
+          <Text>Add Task</Text>
+        </Button>
+        <FlatList
+          data={tasks}
+          keyExtractor={(item, index) => item.id}
+          renderItem={({item, index}) => renderTask(item, index)}
+          style={styles.flatList}
+        />
+      </View>
+      {renderEditTask(editingTask, tasks.findIndex(task => task.id === editingTask?.id))}
+    </>
   );
 }
 
@@ -177,5 +233,34 @@ const styles = StyleSheet.create({
   },
   textInputContent: {
     color: "white",
+  },
+  modalContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+  },
+  modalTitle: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  modalContent: {
+    width: "80%",
+    margin: 20,
+    backgroundColor: 'black',
+    borderRadius: 20,
+    borderWidth: 1,
+    borderColor: 'white',
+    padding: 35,
+    alignItems: 'center',
+    shadowColor: '#fff',
+    shadowOffset: {
+      width: 2,
+      height: 4,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
   }
 });
